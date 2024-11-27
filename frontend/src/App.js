13ac09ddb965f645f32fb6f38ee8b93178ce20c5
Cosmin <timoftecosmin@yahoo.com>
2024-11-27 09:31:53 +0200

    Prepare for GitHub Pages deployment update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,20 +15,20 @@ import kid_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path="product" element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
-        </Route>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
-      </Routes>
-      <Footer/>
+      <BrowserRouter basename="/Ecommerce-React-Project">
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
+          <Route path="product" element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
